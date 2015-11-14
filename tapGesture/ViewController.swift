65201adda9d8b048678993c9266c4ff14f49f992
gouf <innocent.zero@gmail.com>
2015-11-14 11:05:48 +0900

    Initial Commit

---
+++ tapGesture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tapGesture
+//
+//  Created by 古屋 郷 on 2015/11/14.
+//  Copyright © 2015年 古屋 郷. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
